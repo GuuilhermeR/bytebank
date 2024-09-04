e8412055eaf9e22565e1b534ec50cda68a5bd9c2
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-09-04 20:22:57 -0300

    02.Implementando layout com Widgets - Refatorando o código.

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          body: const Column(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100.0'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('200.0'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-            ],
-          ),
+          body: ListaTransferencia(),
           appBar: AppBar(
               title: AppBar(
             title: const Text('Transferências'),
@@ -32,3 +15,42 @@ void main() => runApp(
         ),
       ),
     );
+
+class ListaTransferencia
+    extends StatelessWidget //StatelessWidget é usado para coisas constantes que não mudam. StatefulWidgets são usados para coisas que mudam.
+{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100, 1000)),
+        ItemTransferencia(Transferencia(200, 2000)),
+        ItemTransferencia(Transferencia(300, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
