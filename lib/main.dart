51a1a22a58fcfce4608922afde45ff6cff90166a
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-09-04 21:04:04 -0300

    03.Criando formulário e refatorando o código - Montando o Layout do formulário

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,87 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: ListaTransferencia(),
-          appBar: AppBar(
-              title: AppBar(
-            title: const Text('Transferências'),
-          )),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: const Icon(Icons.add),
-          ),
-        ),
+void main() => runApp(const BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
       ),
     );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  const FormularioTransferencia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criando Transferência'),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Confirmar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class ListaTransferencia
     extends StatelessWidget //StatelessWidget é usado para coisas constantes que não mudam. StatefulWidgets são usados para coisas que mudam.
 {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100, 1000)),
-        ItemTransferencia(Transferencia(200, 2000)),
-        ItemTransferencia(Transferencia(300, 3000)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+          title: AppBar(
+        title: const Text('Transferências'),
+      )),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100, 1000)),
+          ItemTransferencia(Transferencia(200, 2000)),
+          ItemTransferencia(Transferencia(300, 3000)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
